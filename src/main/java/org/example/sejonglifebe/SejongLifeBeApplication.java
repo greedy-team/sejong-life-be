bbb8332e7aa68a954095ae835bed3db91cfca5c8
jihwanyeom <yum020428@gmail.com>
2025-08-11 13:20:38 +0900

    initial commit

---
+++ src/main/java/org/example/sejonglifebe/SejongLifeBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.sejonglifebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SejongLifeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SejongLifeBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sejonglifebe/SejongLifeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sejonglifebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SejongLifeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
