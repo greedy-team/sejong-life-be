982483b0b7dfc3d1a679058cda002c9750e4cfa9
gjtjrl303 <gjtjrl303@naver.com>
2025-08-16 03:21:14 +0900

    refactor: import 추가

--- src/main/java/org/example/sejonglifebe/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/sejonglifebe/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.example.sejonglifebe.common.dto.ErrorResponse;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
