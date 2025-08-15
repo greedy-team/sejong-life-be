1e533121edfd097cc5bf5926c14465136adfceec
gjtjrl303 <gjtjrl303@naver.com>
2025-08-16 03:21:49 +0900

    refactor: 태그 조회 api 하나로 통합

--- src/main/java/org/example/sejonglifebe/tag/TagController.java
+++ src/main/java/org/example/sejonglifebe/tag/TagController.java
@@ -2,10 +2,12 @@ package org.example.sejonglifebe.tag;
 
 import lombok.RequiredArgsConstructor;
 import org.example.sejonglifebe.common.dto.ApiResponse;
+import org.example.sejonglifebe.tag.dto.TagResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,15 +19,9 @@ public class TagController {
 
     private final TagService tagService;
 
-    @GetMapping
-    public ResponseEntity<ApiResponse<List<TagResponse>>> getTags() {
-        List<TagResponse> data = tagService.getAllTags();
-        return ApiResponse.of(HttpStatus.OK, "전체 태그 목록 조회 성공", data);
-    }
-
-    @GetMapping
-    public ResponseEntity<ApiResponse<List<TagResponse>>> getTagsByCategoryId(@RequestParam("categoryId") Long categoryId) {
+    @GetMapping()
+    public ResponseEntity<ApiResponse<List<TagResponse>>> getTags(@RequestParam(value = "categoryId", required = false) Long categoryId) {
         List<TagResponse> tagResponses = tagService.getTagsByCategoryId(categoryId);
-        return ApiResponse.of(HttpStatus.OK, "카테고리별 태그 목록 조회 성공", tagResponses);
+        return ApiResponse.of(HttpStatus.OK, "전체 태그 목록 조회 성공", tagResponses);
     }
 }
