1bcf0a572716edd521a196400400fd2f5be2cd59
gjtjrl303 <gjtjrl303@naver.com>
2025-08-15 20:29:44 +0900

    feat: 전역 예외 처리 및 ErrorCode enum 구현

---
+++ src/main/java/org/example/sejonglifebe/exception/SejongLifeException.java
@@ -0,0 +1,14 @@
+package org.example.sejonglifebe.exception;
+
+import lombok.Getter;
+
+@Getter
+public class SejongLifeException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public SejongLifeException(ErrorCode errorCode) {
+        super(errorCode.getErrorMessage());
+        this.errorCode = errorCode;
+    }
+}
