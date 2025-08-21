ce674ab64932d4d659ddc0134ecb84dadfc32d1a
jihwanyeom <yum020428@gmail.com>
2025-08-21 18:15:01 +0900

    chore : 디버깅 코드 제거

--- deploy.sh
+++ deploy.sh
@@ -26,13 +26,7 @@ echo "Gradle Build"
 chmod +x ./gradlew
 ./gradlew build
 
-# ==================== 여기부터 교체 ====================
-
-echo "--- Starting Debug Mode ---"
-set -x # 실행되는 모든 명령어를 터미널에 + 기호와 함께 보여줍니다.
-
 CURRENT_PID=$(pgrep -f "${JAR_NAME}.*\.jar.*spring\.profiles\.active=$PROFILE" || true)
-echo "Found PID: [$CURRENT_PID]" # 변수 확인을 위해 대괄호로 감쌉니다.
 
 if [ -z "$CURRENT_PID" ]; then
     echo "No running application for $PROFILE"
@@ -46,15 +40,11 @@ echo "Deploy new application for $PROFILE"
 
 jars=(build/libs/*[!plain].jar)
 JAR_FILE=${jars[0]}
-echo "Found JAR file: [$JAR_FILE]" # 변수 확인을 위해 대괄호로 감쌉니다.
 
 mkdir -p "$LOG_DIR"
 LOG_PATH="$LOG_DIR/server_log_${PROFILE}_$(date +%Y-%m-%d).txt"
 
 nohup java -jar -Dspring.profiles.active=$PROFILE "$JAR_FILE" >> "$LOG_PATH" 2>&1 &
-
-set +x # 디버깅 모드 종료
-echo "--- Exiting Debug Mode ---"
 
 find build/libs -type f -name "*.jar" -mtime +3 -delete
 
