064c3a37e6036a78ca612ff3cd2cbda4c26a23f7
gjtjrl303 <gjtjrl303@naver.com>
2025-08-21 14:00:53 +0900

    feat: review repository, service 초기 구현

---
+++ src/main/java/org/example/sejonglifebe/review/ReviewRepository.java
@@ -0,0 +1,8 @@
+package org.example.sejonglifebe.review;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
