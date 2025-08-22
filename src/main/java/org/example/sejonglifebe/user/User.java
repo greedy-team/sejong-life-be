9d58fa6e26ea6aae555cb690cc2227332205d008
Jiwoo <ziuoo0424@gmail.com>
2025-08-22 23:49:54 +0900

    feat: User 엔티티 설계 및 초기 설정 - 연관관계 설정 - Repository, Service 파일 생성

---
+++ src/main/java/org/example/sejonglifebe/user/User.java
@@ -0,0 +1,49 @@
+package org.example.sejonglifebe.user;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.sejonglifebe.review.Review;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "student_id", unique = true, nullable = false)
+    private String studentId;
+
+    @Column(unique = true, nullable = false, length = 15)
+    private String nickname;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    private List<Review> reviews = new ArrayList<>();
+}
