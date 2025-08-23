ea7ff4dcb861c05f16aa8fa9b1aabad66a51e31d
Shin Jihoon <tobeabestns@naver.com>
2025-08-23 15:35:25 +0900

    feat: Roulette DTO 생성

--- src/main/java/org/example/sejonglifebe/roulette/dto/RouletteResponse.java
+++ src/main/java/org/example/sejonglifebe/roulette/dto/RouletteResponse.java
@@ -1,4 +1,19 @@
 package org.example.sejonglifebe.roulette.dto;
 
-public class RouletteResponse {
+import org.example.sejonglifebe.place.entity.Place;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public record RouletteResponse(List<RoulettePlaceDto> places) {
+
+    public static RouletteResponse from(List<Place> places) {
+        List<RoulettePlaceDto> dtoList = places.stream()
+                .map(place -> new RoulettePlaceDto(place.getId(), place.getName()))
+                .collect(Collectors.toList());
+        return new RouletteResponse(dtoList);
+    }
+
+    public record RoulettePlaceDto(Long placeId, String placeName) {
+    }
 }
