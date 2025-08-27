3a22c77fa0c612a869dcbe8fbbdc4b1b19449b75
Jiwoo <ziuoo0424@gmail.com>
2025-08-27 21:08:02 +0900

    style: 개행 추가

--- src/main/java/org/example/sejonglifebe/review/dto/ReviewSummaryResponse.java
+++ src/main/java/org/example/sejonglifebe/review/dto/ReviewSummaryResponse.java
@@ -7,4 +7,5 @@ public record ReviewSummaryResponse(
         Double averageRate,
         Map<String, Long> ratingDistribution
 ) {
+
 }
--- src/main/java/org/example/sejonglifebe/tag/dto/TagResponse.java
+++ src/main/java/org/example/sejonglifebe/tag/dto/TagResponse.java
@@ -6,6 +6,7 @@ public record TagResponse(
         Long tagId,
         String tagName
 ) {
+
     public static TagResponse from(Tag tag) {
         return new TagResponse(tag.getId(), tag.getName());
     }
