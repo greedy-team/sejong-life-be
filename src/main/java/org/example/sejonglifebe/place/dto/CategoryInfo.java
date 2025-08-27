1976c41fafe6afcb7155ae015adc714e838fa52b
Jiwoo <ziuoo0424@gmail.com>
2025-08-27 21:07:30 +0900

    feat: 중복되는 record 메서드 파일로 분리 - PlaceResponse, PlaceDetailResponse에서 모두 사용되기에 재사용하기 위해 파일로 분리함

---
+++ src/main/java/org/example/sejonglifebe/place/dto/CategoryInfo.java
@@ -0,0 +1,13 @@
+package org.example.sejonglifebe.place.dto;
+
+import org.example.sejonglifebe.place.entity.PlaceCategory;
+
+public record CategoryInfo(
+        Long categoryId,
+        String categoryName
+) {
+
+    public static CategoryInfo from(PlaceCategory placeCategory) {
+        return new CategoryInfo(placeCategory.getCategory().getId(), placeCategory.getCategory().getName());
+    }
+}
---
+++ src/main/java/org/example/sejonglifebe/place/dto/TagInfo.java
@@ -0,0 +1,13 @@
+package org.example.sejonglifebe.place.dto;
+
+import org.example.sejonglifebe.place.entity.PlaceTag;
+
+public record TagInfo(
+        Long tagId,
+        String tagName
+) {
+
+    public static TagInfo from(PlaceTag placeTag) {
+        return new TagInfo(placeTag.getTag().getId(), placeTag.getTag().getName());
+    }
+}
