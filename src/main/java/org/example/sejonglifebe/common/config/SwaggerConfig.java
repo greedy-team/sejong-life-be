ce917c878b2a6b7292840ed115024ea63133c1d2
Jiwoo <ziuoo0424@gmail.com>
2025-09-01 00:04:50 +0900

    refactor: SwaggerConfig 서버 하드코딩 제거 및 JWT Authorize 버튼 지원 추가

--- src/main/java/org/example/sejonglifebe/common/config/SwaggerConfig.java
+++ src/main/java/org/example/sejonglifebe/common/config/SwaggerConfig.java
@@ -1,35 +1,36 @@
 package org.example.sejonglifebe.common.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
+@ConfigurationProperties(prefix = "swagger")
 public class SwaggerConfig {
+
+    private final List<Server> servers = new ArrayList<>();
 
     @Bean
     public OpenAPI customOpenAPI() {
-        Server localServer = new Server();
-        localServer.setUrl("http://localhost:8080");
-        localServer.setDescription("Local Server");
-
-        Server devServer = new Server();
-        devServer.setUrl("http://3.37.62.143:8081");
-        devServer.setDescription("Development Server");
-
-        Server apiServer = new Server();
-        apiServer.setUrl("https://sejong-life.site");
-        apiServer.setDescription("Production Server");
 
         return new OpenAPI()
-                .info(new Info()
-                        .title("SejongLife API")
-                        .version("1.0")
-                        .description("SejongLife API v1"))
-                .servers(Arrays.asList(localServer, apiServer));
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .info(new Info().title("SejongLife API").version("1.0").description("SejongLife API v1"))
+                .servers(servers);
     }
 }
