8d2752de20f2ece5897faccd9c65c10aa1f60fd8
jihwanyeom <yum020428@gmail.com>
2025-09-01 18:03:59 +0900

    chore: swagger 경로 설정 변경

--- src/main/java/org/example/sejonglifebe/SejongLifeBeApplication.java
+++ src/main/java/org/example/sejonglifebe/SejongLifeBeApplication.java
@@ -1,5 +1,7 @@
 package org.example.sejonglifebe;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class SejongLifeBeApplication {
 
     public static void main(String[] args) {
