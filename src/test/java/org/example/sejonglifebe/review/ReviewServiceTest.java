7b734b43fca98670b80e49604b850c2682b4bcf7
Jiwoo <ziuoo0424@gmail.com>
2025-09-04 23:25:54 +0900

    refactor: 임포트문 와일드카드 제거

--- src/test/java/org/example/sejonglifebe/review/ReviewServiceTest.java
+++ src/test/java/org/example/sejonglifebe/review/ReviewServiceTest.java
@@ -26,7 +26,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
