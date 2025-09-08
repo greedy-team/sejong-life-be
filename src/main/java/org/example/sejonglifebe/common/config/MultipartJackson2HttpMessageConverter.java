a3335c9e23a210e9be455980749635359a83b2fb
gjtjrl303 <gjtjrl303@naver.com>
2025-09-09 00:21:29 +0900

    refactor:MappingJackson2HttpMessageConverte -> AbstractHttpMessageConverter 상속으로 변경

--- src/main/java/org/example/sejonglifebe/common/config/MultipartJackson2HttpMessageConverter.java
+++ src/main/java/org/example/sejonglifebe/common/config/MultipartJackson2HttpMessageConverter.java
@@ -2,23 +2,38 @@ package org.example.sejonglifebe.common.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.sejonglifebe.review.dto.ReviewRequest;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.lang.Nullable;
+import org.springframework.http.converter.AbstractHttpMessageConverter;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.io.IOException;
 
 @Component
-public class MultipartJackson2HttpMessageConverter extends MappingJackson2HttpMessageConverter {
+public class MultipartJackson2HttpMessageConverter extends AbstractHttpMessageConverter<Object> {
+
+    private final ObjectMapper objectMapper;
 
     public MultipartJackson2HttpMessageConverter(ObjectMapper objectMapper) {
-        super(objectMapper);
-        setSupportedMediaTypes(List.of(MediaType.APPLICATION_JSON, MediaType.APPLICATION_OCTET_STREAM, MediaType.MULTIPART_FORM_DATA));
+        super(MediaType.APPLICATION_JSON, MediaType.APPLICATION_OCTET_STREAM);
+        this.objectMapper = objectMapper;
     }
 
     @Override
-    public boolean canRead(Class<?> clazz, @Nullable MediaType mediaType) {
-        return ReviewRequest.class.isAssignableFrom(clazz);
+    protected boolean supports(Class<?> clazz) {
+        return clazz == ReviewRequest.class;
+    }
+
+    @Override
+    protected Object readInternal(Class<?> clazz, HttpInputMessage inputMessage)
+            throws IOException {
+        return objectMapper.readValue(inputMessage.getBody(), clazz);
+    }
+
+    @Override
+    protected void writeInternal(Object o, HttpOutputMessage outputMessage)
+            throws IOException {
+        outputMessage.getBody().write(objectMapper.writeValueAsBytes(o));
     }
 }
