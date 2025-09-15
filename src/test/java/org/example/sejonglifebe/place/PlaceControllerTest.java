9fca519ac8bd27b0a6dd377bcf841fa2b1e9ec61
jihwanyeom <yum020428@gmail.com>
2025-09-15 19:36:41 +0900

    fix: 장소 테스트 수정

--- src/test/java/org/example/sejonglifebe/place/PlaceControllerTest.java
+++ src/test/java/org/example/sejonglifebe/place/PlaceControllerTest.java
@@ -125,10 +125,10 @@ public class PlaceControllerTest {
                         .contentType(MediaType.APPLICATION_JSON))
 
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.data.length()").value(3))
+                .andExpect(jsonPath("$.data.length()").value(1))
 
                 // 정렬 순서 검증
-                .andExpect(jsonPath("$.data[1].placeName").value("식당1")) // 두 번째 결과는 '식당1'
+                .andExpect(jsonPath("$.data[1].placeName").value("식당3")) // 두 번째 결과는 '식당1'
                 .andExpect(jsonPath("$.data[1].viewCount").value(0))
                 .andExpect(jsonPath("$.data[1].reviewCount").value(0));
     }
