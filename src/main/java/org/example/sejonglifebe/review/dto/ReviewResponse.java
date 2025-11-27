6ba10f14642ccfcbf0f14f1face22ade6ba85b6f
gjtjrl303 <gjtjrl303@naver.com>
2025-11-27 16:13:59 +0900

    fix: 학번 응답 형식 변경

--- src/main/java/org/example/sejonglifebe/review/dto/ReviewResponse.java
+++ src/main/java/org/example/sejonglifebe/review/dto/ReviewResponse.java
@@ -5,9 +5,7 @@ import java.util.List;
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.example.sejonglifebe.common.dto.TagInfo;
 import org.example.sejonglifebe.place.dto.PlaceImageInfo;
-import org.example.sejonglifebe.place.entity.PlaceImage;
 import org.example.sejonglifebe.review.Review;
-import software.amazon.awssdk.services.s3.endpoints.internal.Value.Bool;
 
 @Schema(description = "리뷰 응답")
 public record ReviewResponse(
@@ -15,7 +13,7 @@ public record ReviewResponse(
         @Schema(description = "별점", example = "5") int rating,
         @Schema(description = "작성자 ID", example = "123") Long userId,
         @Schema(description = "작성자 닉네임", example = "카페덕후") String userName,
-        @Schema(description = "작성자 학번", example = "20230001") String studentId,
+        @Schema(description = "작성자 학번", example = "20") String studentId,
         @Schema(description = "리뷰 내용", example = "커피가 진하고 맛있어요!") String content,
         @Schema(description = "좋아요 수", example = "7") Long likeCount,
         @Schema(description = "작성일시(ISO 문자열)", example = "2025-08-24T13:45:00") String createdAt,
@@ -31,7 +29,7 @@ public record ReviewResponse(
                 review.getRating(),
                 review.getUser().getId(),
                 review.getUser().getNickname(),
-                review.getUser().getStudentId(),
+                review.getUser().getStudentId().substring(0,2),
                 review.getContent(),
                 review.getLikeCount(),
                 review.getCreatedAt().toString(),
