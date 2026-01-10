357b6e3f262ccf44b80a9d67839dc9737443f43f
gjtjrl303 <gjtjrl303@naver.com>
2026-01-10 11:28:08 +0900

    feat: 장소 조회 로직 querydsl 로 변경 및 검색어 조건 추가

---
+++ src/main/java/org/example/sejonglifebe/place/PlaceRepositoryImpl.java
@@ -0,0 +1,69 @@
+package org.example.sejonglifebe.place;
+
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.example.sejonglifebe.category.Category;
+import org.example.sejonglifebe.place.entity.Place;
+import org.example.sejonglifebe.tag.Tag;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+import static org.example.sejonglifebe.place.entity.QPlace.*;
+import static org.example.sejonglifebe.place.entity.QPlaceCategory.*;
+import static org.example.sejonglifebe.place.entity.QPlaceTag.*;
+import static org.example.sejonglifebe.review.QReview.*;
+
+@RequiredArgsConstructor
+public class PlaceRepositoryImpl implements PlaceRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<Place> getPlacesByConditions(Category category, List<Tag> tags, String keyword) {
+        return queryFactory.selectFrom(place)
+                .distinct()
+                .leftJoin(place.placeCategories, placeCategory)
+                .leftJoin(place.placeTags, placeTag)
+                .leftJoin(place.reviews, review)
+                .where(likePlaceName(keyword),
+                        placeCategoryEq(category),
+                        placeTagIn(tags)
+                )
+                .groupBy(place.id)
+                .having(placeTagCountEq(tags))
+                .orderBy(review.count().desc())
+                .fetch();
+
+    }
+
+    private BooleanExpression likePlaceName(String keyword) {
+        if (!StringUtils.hasText(keyword)) {
+            return null;
+        }
+        return place.name.like("%" + keyword + "%");
+    }
+
+    private BooleanExpression placeCategoryEq(Category category) {
+        if (category == null) {
+            return null;
+        }
+        return placeCategory.category.eq(category);
+    }
+
+    private Predicate placeTagIn(List<Tag> tags) {
+        if (tags == null || tags.isEmpty()) {
+            return null;
+        }
+        return placeTag.tag.in(tags);
+    }
+
+    private Predicate placeTagCountEq(List<Tag> tags) {
+        if (tags == null || tags.isEmpty()) {
+            return null;
+        }
+        return placeTag.tag.countDistinct().eq((long) tags.size());
+    }
+}
