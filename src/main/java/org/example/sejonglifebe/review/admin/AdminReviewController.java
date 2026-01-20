e2c5cf069bff7961af890da9e9ed390b12d22bbf
gjtjrl303 <gjtjrl303@naver.com>
2026-01-20 16:12:05 +0900

    feat: 관리자 리뷰 로그 api 구현

---
+++ src/main/java/org/example/sejonglifebe/review/admin/AdminReviewController.java
@@ -0,0 +1,32 @@
+package org.example.sejonglifebe.review.admin;
+
+import lombok.RequiredArgsConstructor;
+import org.example.sejonglifebe.auth.AuthUser;
+import org.example.sejonglifebe.auth.LoginRequired;
+import org.example.sejonglifebe.common.dto.CommonResponse;
+import org.example.sejonglifebe.review.ReviewService;
+import org.example.sejonglifebe.review.admin.dto.AdminReviewResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin")
+public class AdminReviewController {
+
+    private final ReviewService reviewService;
+    private final SseService sseService;
+
+    @LoginRequired
+    @GetMapping("/reviews")
+    public ResponseEntity<CommonResponse<List<AdminReviewResponse>>> getAdminReviews(AuthUser authUser) {
+        return CommonResponse.of(HttpStatus.OK, "리뷰 로그 목록 조회 성공", reviewService.findAllReviews());
+    }
+}
