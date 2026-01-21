c021c3507219c149f77c29c13aa52b7dc0ba3ed7
jihwanyeom <yum020428@gmail.com>
2026-01-21 21:32:45 +0900

    refactor : swagger API 명 명시

--- src/main/java/org/example/sejonglifebe/place/PlaceControllerSwagger.java
+++ src/main/java/org/example/sejonglifebe/place/PlaceControllerSwagger.java
@@ -5,8 +5,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import org.example.sejonglifebe.auth.AuthUser;
 import org.example.sejonglifebe.common.dto.CommonResponse;
 import org.example.sejonglifebe.place.dto.PlaceDetailResponse;
+import org.example.sejonglifebe.place.dto.PlaceRequest;
 import org.example.sejonglifebe.place.dto.PlaceResponse;
 import org.example.sejonglifebe.place.dto.PlaceSearchConditions;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name = "Place", description = "장소")
 public interface PlaceControllerSwagger {
@@ -30,4 +34,17 @@ public interface PlaceControllerSwagger {
             @PathVariable Long placeId,
             HttpServletRequest request,
             HttpServletResponse response);
+
+    @Operation(summary = "주간 핫플레이스 조회")
+    ResponseEntity<CommonResponse<Void>> createPlace(
+            @Valid @RequestPart("place") PlaceRequest placeRequest,
+            @RequestPart(value = "thumbnail", required = false) MultipartFile thumbnail,
+            AuthUser authUser
+    );
+
+    @Operation(summary = "주간 핫플레이스 조회")
+    ResponseEntity<CommonResponse<Void>> deletePlace(
+            @PathVariable Long placeId,
+            AuthUser authUser
+    );
 }
