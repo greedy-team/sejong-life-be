768147de758d602c4ef13a44f5814867da664c8a
jihwanyeom <yum020428@gmail.com>
2026-01-21 18:14:33 +0900

    feat : 장소 생성 API Controller 및 Service 계층 구현

--- src/main/java/org/example/sejonglifebe/place/PlaceController.java
+++ src/main/java/org/example/sejonglifebe/place/PlaceController.java
@@ -4,20 +4,28 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.example.sejonglifebe.auth.AuthUser;
+import org.example.sejonglifebe.auth.LoginRequired;
 import org.example.sejonglifebe.common.dto.CommonResponse;
 import org.example.sejonglifebe.place.dto.PlaceDetailResponse;
+import org.example.sejonglifebe.place.dto.PlaceRequest;
 import org.example.sejonglifebe.place.dto.PlaceSearchConditions;
+import org.example.sejonglifebe.review.dto.ReviewRequest;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
 import org.example.sejonglifebe.place.dto.PlaceResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -46,5 +54,16 @@ public class PlaceController implements PlaceControllerSwagger {
             HttpServletResponse response) {
         PlaceDetailResponse placeDetailResponse = placeService.getPlaceDetail(placeId, request, response);
         return CommonResponse.of(HttpStatus.OK, "장소 상세 정보 조회 성공", placeDetailResponse);
+    }
+
+    @LoginRequired
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<CommonResponse<Void>> createPlace(
+            @Valid @RequestPart("place") PlaceRequest placeRequest,
+            @RequestPart(value = "thumbnail", required = false) MultipartFile thumbnail,
+            AuthUser authUser
+    ) {
+        placeService.createPlace(placeRequest, thumbnail, authUser);
+        return CommonResponse.of(HttpStatus.CREATED, "장소 추가 성공", null);
     }
 }
