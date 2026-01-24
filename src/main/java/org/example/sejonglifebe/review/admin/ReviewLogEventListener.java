633baa24ad5d114ba193f08572f7ed447876be77
gjtjrl303 <gjtjrl303@naver.com>
2026-01-24 14:47:03 +0900

    refactor: @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT) 추가해서 db에 커밋이 된 경우만 이벤트 발행

--- src/main/java/org/example/sejonglifebe/review/admin/ReviewLogEventListener.java
+++ src/main/java/org/example/sejonglifebe/review/admin/ReviewLogEventListener.java
@@ -3,9 +3,10 @@ package org.example.sejonglifebe.review.admin;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.sejonglifebe.review.admin.dto.AdminReviewResponse;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
@@ -15,7 +16,7 @@ public class ReviewLogEventListener {
     private final SseService sseService;
 
     @Async
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleReviewLogEvent(AdminReviewResponse reviewData) {
         log.info("리뷰 생성 이벤트 수신: reviewId={}, placeName={}", reviewData.reviewId(), reviewData.placeName());
         sseService.sendToAll("review-created", reviewData);
