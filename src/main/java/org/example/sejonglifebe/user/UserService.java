f02a936499b714bf969b317f5b1d897833d0128d
jihwanyeom <yum020428@gmail.com>
2026-01-29 14:07:45 +0900

    feat : 유저 정보 업데이트 및 마이페이지 정보 조회 메서드 작성

--- src/main/java/org/example/sejonglifebe/user/UserService.java
+++ src/main/java/org/example/sejonglifebe/user/UserService.java
@@ -2,13 +2,18 @@ package org.example.sejonglifebe.user;
 
 import lombok.RequiredArgsConstructor;
 import org.example.sejonglifebe.auth.AuthUser;
+import org.example.sejonglifebe.auth.PortalStudentInfo;
 import org.example.sejonglifebe.common.jwt.JwtTokenProvider;
 import org.example.sejonglifebe.exception.ErrorCode;
 import org.example.sejonglifebe.exception.SejongLifeException;
+import org.example.sejonglifebe.place.PlaceRepository;
+import org.example.sejonglifebe.place.favorite.FavoritePlaceRepository;
+import org.example.sejonglifebe.place.favorite.FavoritePlaceService;
 import org.example.sejonglifebe.review.Review;
 import org.example.sejonglifebe.review.ReviewLikeRepository;
 import org.example.sejonglifebe.review.ReviewRepository;
 import org.example.sejonglifebe.s3.S3Service;
+import org.example.sejonglifebe.user.dto.MyPageResponse;
 import org.example.sejonglifebe.user.dto.SignUpRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,6 +29,7 @@ public class UserService {
     private final JwtTokenProvider jwtTokenProvider;
     private final ReviewRepository reviewRepository;
     private final ReviewLikeRepository reviewLikeRepository;
+    private final FavoritePlaceRepository favoritePlaceRepository;
     private final S3Service s3Service;
 
     @Transactional(readOnly = true)
@@ -32,14 +38,16 @@ public class UserService {
     }
 
     @Transactional
-    public String createUser(SignUpRequest requestDto) {
+    public String createUser(SignUpRequest requestDto, PortalStudentInfo portalStudentInfo) {
         if (userRepository.existsByNickname(requestDto.getNickname())) {
             throw new SejongLifeException(ErrorCode.DUPLICATE_NICKNAME);
         }
 
         User newUser = User.builder()
-                .studentId(requestDto.getStudentId())
+                .studentId(portalStudentInfo.getStudentId())
                 .nickname(requestDto.getNickname())
+                .name(portalStudentInfo.getName())
+                .department(portalStudentInfo.getDepartment())
                 .build();
 
         User savedUser = userRepository.save(newUser);
@@ -63,4 +71,44 @@ public class UserService {
         reviewLikeRepository.deleteAllByUser(user);
         userRepository.delete(user);
     }
+
+    @Transactional
+    public void updateStudentProfileIfChanged(Long userId, String name, String department) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new SejongLifeException(ErrorCode.USER_NOT_FOUND));
+
+        boolean changed = false;
+
+        if (name != null && !name.isBlank() && !name.equals(user.getName())) {
+            changed = true;
+        }
+        if (department != null && !department.isBlank() && !department.equals(user.getDepartment())) {
+            changed = true;
+        }
+
+        if (changed) {
+            user.updateStudentProfile(name, department);
+        }
+    }
+
+    @Transactional
+    public MyPageResponse getMyPageInfo(AuthUser authUser) {
+        User user = userRepository.findByStudentId(authUser.studentId())
+                .orElseThrow(() -> new SejongLifeException(ErrorCode.USER_NOT_FOUND));
+        String studentId = user.getStudentId().substring(0,2);
+        String name = user.getName();
+        String nickname = user.getNickname();
+        String department = user.getDepartment();
+        long favoriteCount = favoritePlaceRepository.countByUserStudentId(studentId);
+        long reviewCount = reviewRepository.countByUserStudentId(studentId);
+        return new MyPageResponse(
+                name,
+                nickname,
+                studentId,
+                department,
+                favoriteCount,
+                reviewCount
+        );
+    }
+
 }
