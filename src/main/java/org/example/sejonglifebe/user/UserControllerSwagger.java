6ade9ce12163512147de707c509edaa99fd00664
jihwanyeom <yum020428@gmail.com>
2026-01-29 14:08:10 +0900

    feat : 마이페이지 조회 API 작성 및 Swagger 수정

--- src/main/java/org/example/sejonglifebe/user/UserControllerSwagger.java
+++ src/main/java/org/example/sejonglifebe/user/UserControllerSwagger.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.example.sejonglifebe.auth.AuthUser;
 import org.example.sejonglifebe.common.dto.CommonResponse;
+import org.example.sejonglifebe.user.dto.MyPageResponse;
 import org.example.sejonglifebe.user.dto.SignUpRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,4 +21,7 @@ public interface UserControllerSwagger {
 
     @Operation(summary = "회원 탈퇴", description = "회원 탈퇴 시 작성한 리뷰와 좋아요가 모두 삭제됩니다")
     ResponseEntity<CommonResponse<Void>> deleteUser(AuthUser authUser);
+
+    @Operation(summary = "회원 마이페이지 정보 조회", description = "회원 마이페이지 정보를 불러옵니다")
+    public ResponseEntity<CommonResponse<MyPageResponse>> getMyPageInfo(AuthUser authUser);
 }
