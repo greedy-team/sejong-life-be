21aca45880ad5dd541e627f3b7e3377199fde14d
gjtjrl303 <gjtjrl303@naver.com>
2026-05-18 23:40:08 +0900

    test: 보너스 열람권 있어도 기본 열람권을 먼저 사용하는 테스트 케이스 추가

--- src/test/java/org/example/sejonglifebe/meeting/MeetingProfileServiceTest.java
+++ src/test/java/org/example/sejonglifebe/meeting/MeetingProfileServiceTest.java
@@ -273,6 +273,50 @@ class MeetingProfileServiceTest {
         }
 
         @Test
+        @DisplayName("보너스 열람권이 있어도 기본 열람권이 있으면 기본 열람권을 먼저 사용한다")
+        void openContact_success_basicUsedBeforeBonus() {
+            MeetingProfile requester = MeetingProfile.builder()
+                    .kakaoId("kakao-1")
+                    .gender(Gender.MALE)
+                    .faceType(FaceType.DOG)
+                    .birthYear(2000)
+                    .hobby("축구")
+                    .dateStyle("활동적인 데이트")
+                    .contact("requester_contact")
+                    .bonusOpenCount(1)
+                    .build();
+
+            MeetingProfile target = MeetingProfile.builder()
+                    .kakaoId("kakao-2")
+                    .gender(Gender.FEMALE)
+                    .faceType(FaceType.CAT)
+                    .birthYear(2001)
+                    .hobby("영화")
+                    .dateStyle("조용한 데이트")
+                    .contact("insta_contact")
+                    .bonusOpenCount(0)
+                    .build();
+
+            ReflectionTestUtils.setField(requester, "id", 1L);
+            ReflectionTestUtils.setField(target, "id", 2L);
+
+            MeetingAuthUser meetingAuthUser = new MeetingAuthUser("kakao-1");
+
+            given(meetingProfileRepository.findByKakaoIdWithLock("kakao-1")).willReturn(Optional.of(requester));
+            given(meetingProfileRepository.findById(2L)).willReturn(Optional.of(target));
+            given(contactViewHistoryRepository.existsByViewerIdAndTargetId(1L, 2L)).willReturn(false);
+            given(meetingOpenCountService.isRechargeable("kakao-1")).willReturn(true);
+
+            meetingProfileService.openContact(meetingAuthUser, 2L);
+
+            assertThat(requester.getBonusOpenCount()).isEqualTo(1);
+
+            ArgumentCaptor<CooldownStartEvent> captor = ArgumentCaptor.forClass(CooldownStartEvent.class);
+            verify(eventPublisher).publishEvent(captor.capture());
+            assertThat(captor.getValue().kakaoId()).isEqualTo("kakao-1");
+        }
+
+        @Test
         @DisplayName("열람권이 없고 쿨다운 중이면 예외를 던진다")
         void openContact_fail_insufficientOpenCount() {
             MeetingProfile requester = MeetingProfile.builder()
